416f89d87b553776bae0a3b461f724c3d65b6857
mpun1426 <mpun1426@gmail.com>
2023-05-30 01:41:21 +0900

    2-9, 2-10

--- work/index.php
+++ work/index.php
@@ -1,73 +1,25 @@
 <?php
-  $n = mt_rand(1, 3);
-  // $message = "こんにちは！！".date('l');
-  if ($n == 1) {
-    $message = "見習い中です！".date('l');
-  } elseif ($n == 2) {
-    $message = "勉強中です！".date('l');
-  } else {
-    $message = "こんにちは！！".date('l');
-  };
+    include('_header.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <title>太郎のポートフォリオサイト</title>
-    <link rel="icon" href="favicon.ico">
-    <meta name="description" content="太郎のポートフォリオサイトです。">
-    <link rel="stylesheet" href="css/styles.css">
-  </head>
-  <body>
-    <header>
-      <div class="container">
-        <div class="icon">
-          <img src="img/taro.png" width="120" height="120" alt="太郎のアイコンです">
-        </div>
-        <div class="info">
-          <h1>山田太郎</h1>
-          <p>
-            <?= $message; ?>
-          </p>
-          <ul>
-            <li>
-              <a href="https://dotinstall.com" target="_blank">
-                <img src="img/blog.png" width="20" height="20" alt="ブログサイトへのリンク画像です">
-              </a>
-            </li>
-            <li>
-              <a href="https://dotinstall.com" target="_blank">
-                <img src="img/photos.png" width="20" height="20" alt="写真サイトへのリンク画像です">
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </header>
-    <section class="works">
-      <h1>WORKS</h1>
-      <section>
-        <img src="img/work1.png" width="400" height="260" alt="勇者ゲームの紹介画像">
-        <h1>勇者ゲーム</h1>
-        <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
-        <a href="hero.php">もっと見る</a>
-      </section>
-      <section>
-        <img src="img/work2.png" width="400" height="260" alt="宝探しゲームの紹介画像">
-        <h1>宝探しゲーム</h1>
-        <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
-        <a href="treasure.php">もっと見る</a>
-      </section>
-      <section>
-        <img src="img/work3.png" width="400" height="260" alt="神経衰弱の紹介画像">
-        <h1>神経衰弱</h1>
-        <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
-        <a href="memory.php">もっと見る</a>
-      </section>
-    </section>
-    <footer>
-      <p>(c) dotinstall.com</p>
-    </footer>
-  </body>
-</html>
+<section>
+    <img src="img/work1.png" width="400" height="260" alt="勇者ゲームの紹介画像">
+    <h1>勇者ゲーム</h1>
+    <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+    <a href="hero.php">もっと見る</a>
+</section>
+<section>
+    <img src="img/work2.png" width="400" height="260" alt="宝探しゲームの紹介画像">
+    <h1>宝探しゲーム</h1>
+    <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+    <a href="treasure.php">もっと見る</a>
+</section>
+<section>
+    <img src="img/work3.png" width="400" height="260" alt="神経衰弱の紹介画像">
+    <h1>神経衰弱</h1>
+    <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+    <a href="memory.php">もっと見る</a>
+</section>
+
+<?php
+    include('_footer.php');
